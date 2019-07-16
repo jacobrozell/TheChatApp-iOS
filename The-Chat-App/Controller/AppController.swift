8b0509ad7d6a185548b9f6d27928ce192ec15233
Jacob Rozell <jrozell@myocv.com>
2019-07-16 17:47:33 -0500

    add gitignore

---
+++ The-Chat-App/Controller/AppController.swift
@@ -0,0 +1,36 @@
+//
+//  AppController.swift
+//  The-Chat-App
+//
+//  Created by Jacob Rozell on 7/16/19.
+//  Copyright © 2019 Jacob Rozell. All rights reserved.
+//
+
+import UIKit
+import SnapKit
+
+class AppController: UIViewController {
+    
+    let tableView = MessagesTableView(style: .plain)
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nil, bundle: nil)
+        self.title = "Messages"
+        tableView.backgroundColor = .orange
+        
+        
+        self.view.addSubview(tableView)
+        setup()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        print("init(coder:) has not been implemented")
+    }
+    
+    func setup() {
+        tableView.snp.makeConstraints { (make) in
+            make.edges.equalToSuperview()
+        }
+    }
+}
